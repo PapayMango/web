9251a835f0885a2a886f49f85cf9a1687682ea47
Ishikawa Tatsuki <papaymango@gmail.com>
2022-12-20 16:15:33 +0900

    create components directory

---
+++ components/A_list.tsx
@@ -0,0 +1,43 @@
+import './A_list.css'
+import Article from './Article'
+import mysql from 'mysql2'
+import { GetStaticProps,NextPage } from 'next'
+
+type ArticleProps = React.ComponentProps<typeof Article>
+type A_listProps = {
+    articles:ArticleProps[]
+} 
+
+// let db = {
+//     host:'localhost',
+//     user:'user',
+//     password:'web',
+//     database:'web'
+// }
+
+// function A_list(props:Array<ArticleProps>):NextPage<Array<ArticleProps>>{
+function A_list({articles}:A_listProps){
+    let list = []
+    // for(let i of props){
+    for(let i of articles){
+        list.push(<Article {...i} />)
+    }
+    return (
+        <main>
+            {list}
+        </main>
+    )
+}
+
+// export const getStaticProps:GetStaticProps<{articles:ArticleProps[]}> = async (context) => {
+//     let articles:ArticleProps[] = []
+//     let connection = mysql.createConnection(db)
+//     console.log(connection)
+//     return {
+//         props:{
+//             articles,
+//         },
+//     }
+// }
+
+export default A_list
